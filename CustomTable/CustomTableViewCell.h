8470c468e704ab5f68bddb366a924cfc329c4497
Bianca Binstock <bianca.pr@gmail.com>
2015-04-14 21:59:32 -0400

    commit before final code change p.68

---
+++ CustomTable/CustomTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  CustomTableViewCell.h
+//  CustomTable
+//
+//  Created by Bianca Binstock on 2015-04-14.
+//  Copyright (c) 2015 Bianca Binstock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomTableViewCell : UITableViewCell
+@property (nonatomic, weak) IBOutlet UILabel *nameLabel;
+@property (nonatomic, weak) IBOutlet UILabel *prepTimeLabel;
+@property (nonatomic, weak) IBOutlet UIImageView *thumbnailImageView;
+@end
---
+++ CustomTable/CustomTableViewCell.m
@@ -0,0 +1,13 @@
+//
+//  CustomTableViewCell.m
+//  CustomTable
+//
+//  Created by Bianca Binstock on 2015-04-14.
+//  Copyright (c) 2015 Bianca Binstock. All rights reserved.
+//
+
+#import "CustomTableViewCell.h"
+
+@implementation CustomTableViewCell
+
+@end
