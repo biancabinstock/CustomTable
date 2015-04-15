e280c88754005d094764a404dd1d0f699488e28d
Bianca Binstock <bianca.pr@gmail.com>
2015-04-14 21:20:32 -0400

    custom thumbnails array added

--- CustomTable/CustomTableViewController.m
+++ CustomTable/CustomTableViewController.m
@@ -12,14 +12,21 @@
 
 {
     NSArray *recipeNames;
+    NSArray *recipeImages;
 }
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     recipeNames = @[@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast",
                     @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut",
-                    @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQPork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini"];
-}
+                    @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQPork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry BirdsCake", @"Ham and Cheese Panini"];
+    
+    recipeImages = @[@"egg_benedict.jpg", @"mushroom_risotto.jpg", @"full_breakfast.jpg",
+                                     @"hamburger.jpg", @"ham_and_egg_sandwich.jpg", @"creme_brelee.jpg",
+                                     @"white_chocolate_donut.jpg", @"starbucks_coffee.jpg", @"vegetable_curry.jpg",
+                                     @"instant_noodle_with_egg.jpg", @"noodle_with_bbq_pork.jpg",
+                                     @"japanese_noodle_with_pork.jpg", @"green_tea.jpg", @"thai_shrimp_cake.jpg",
+                                     @"angry_birds_cake.jpg", @"ham_and_cheese_panini.jpg"];
+                    }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
@@ -31,7 +38,7 @@
     static NSString *cellIdentifier = @"Cell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
     cell.textLabel.text = [recipeNames objectAtIndex:indexPath.row];
-    cell.imageView.image = [UIImage imageNamed:@"creme_brelee"];
+    cell.imageView.image = [UIImage imageNamed:[recipeImages objectAtIndex:indexPath.row]];
     return cell;
 }
                     
