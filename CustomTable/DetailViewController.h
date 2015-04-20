478f5ee93e1aebf8a3c5d3cb836f76adfd1e0cfa
Bianca Binstock <bianca.pr@gmail.com>
2015-04-20 15:30:59 -0400

    chap10start

---
+++ CustomTable/DetailViewController.h
@@ -0,0 +1,14 @@
+//
+//  DetailViewController.h
+//  CustomTable
+//
+//  Created by Bianca Binstock on 2015-04-20.
+//  Copyright (c) 2015 Bianca Binstock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (nonatomic, strong) IBOutlet UILabel *recipeLabel;
+@end
