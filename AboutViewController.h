f16e0bbc87a3bc4c45354dc133436b70c524aacd
Bianca Binstock <bianca.pr@gmail.com>
2015-04-23 15:34:02 -0400

    with web link

---
+++ AboutViewController.h
@@ -0,0 +1,15 @@
+//
+//  AboutViewController.h
+//  CustomTable
+//
+//  Created by Bianca Binstock on 2015-04-23.
+//  Copyright (c) 2015 Bianca Binstock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@property (nonatomic, strong) IBOutlet UIWebView *webView;
+
+@end
---
+++ AboutViewController.m
@@ -0,0 +1,44 @@
+//
+//  AboutViewController.m
+//  CustomTable
+//
+//  Created by Bianca Binstock on 2015-04-23.
+//  Copyright (c) 2015 Bianca Binstock. All rights reserved.
+//
+
+#import "AboutViewController.h"
+
+@interface AboutViewController ()
+
+@end
+
+@implementation AboutViewController
+
+
+    - (void)viewDidLoad {
+        [super viewDidLoad];
+        // Add code to load web content in UIWebView
+       // NSURL *url = [NSURL fileURLWithPath:[[NSBundle mainBundle]pathForResource:@"about.html" ofType:nil]];
+        NSURL *url = [NSURL URLWithString:@"http://www.google.ca"];
+        NSURLRequest *request = [NSURLRequest requestWithURL:url];
+        [self.webView loadRequest:request];
+    }
+
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
